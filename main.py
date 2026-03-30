272eac92a3e386ae58b89cbd17af35fa41696e1d
LuezLopis <ltalopez@outlook.com>
2026-03-30 15:48:38 -0500

    set up basic structure of the project with file input reading

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import sys
 
-public class Main 
-{
-    public static void main(String[] args) 
-    {
-        System.out.println("Hello, World!");
-    }
-}
+
+
+def mainProof():
+
+    def __init__(self, input_file):
+        self.input_file = input_file
+
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <InputFile>")
+        sys.exit(1)
+    input_file = sys.argv[1]
+    mainProof(input_file)
